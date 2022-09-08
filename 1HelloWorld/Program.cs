cd9fb1f14b72a4d5c5d5ab8dd3119bef19de2e00
Simon Blümel <simon.b62@gso.schule.koeln>
2022-09-08 12:06:45 +0200

    Aufgabe 3 erledigt

--- 1HelloWorld/Program.cs
+++ 1HelloWorld/Program.cs
@@ -16,3 +16,15 @@ Console.WriteLine(nachname);
 
 Console.WriteLine(alter);
 
+//Aufgabe 3
+
+Console.WriteLine("Vorname eingeben");
+string vornamee = Console.ReadLine();
+
+Console.WriteLine("Alter eingeben");
+string alterr = Console.ReadLine();
+
+Console.WriteLine("Klasse eingeben");
+string klasse = Console.ReadLine();
+
+Console.WriteLine($"{vornamee} ist {alterr} Jahre alt und besucht die {klasse} des Georg - Simon - Ohm Berufskollegs.");
